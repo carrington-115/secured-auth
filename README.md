decc5a0fa4e8ca74e02756de7c6a68d3b02b571e
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-05 10:48:19 +0530

    wrote the code for the models/user.js file Wrote 3 functions and exported from the file Wrote the start docs of the contribution instructions and function docs headings in the readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # secured-auth
+
+**Instructions to contribute**
+
+- Clone the project with the `repo-url`.
+- Run `npm install` to install all the node dependencies.
+- Create your branch, checkout to your branch and run write your code. When you're done upload your code as a pull request.
+
+```bash
+# cloning
+git clone <repo-url>
+
+# installing dependencies
+npm install
+
+# contributing
+git checkout -b <branch-name>
+git commit -m <contribution message>
+git push -u origin <branch-name>
+```
+
+## documentation
+
+### User models function documentation
+
+1. **connectDB**
+
+- **_description_**:
+- **_parameters and returns_**
+- **_Usage_**
+- **_Raises_**
+
+2. **getUserById**:
+
+- **_description_**:
+- **_parameters and returns_**
+- **_Usage_**
+- **_Raises_**
+
+3. **getUserByUsername**:
+
+- **_description_**:
+- **_parameters and returns_**
+- **_Usage_**
+- **_Raises_**
--- models/user.js
+++ models/user.js
@@ -1,1 +1,77 @@
-console.log("hello world");
+require("dotenv").config({ path: "../.env" });
+const { MongoClient, ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
+
+// setting up the database params
+const client = new MongoClient(process.env.MONGODB_API);
+const clientSession = client.startSession();
+let db;
+const dbName = "Accounts";
+
+const connectDB = async () => {
+  try {
+    if (!db) {
+      await client.connect();
+      db = client.db(dbName);
+    }
+    return db;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getUserByUsername = async (username) => {
+  clientSession.startTransaction();
+  try {
+    const db = await connectDB();
+    const user = db.collection("users").aggregate([
+      {
+        $match: { name: username },
+      },
+    ]);
+    await clientSession.commitTransaction();
+    if (user) {
+      return user;
+    } else {
+      return `The user: ${username} does not exist`;
+    }
+  } catch (error) {
+    await clientSession.abortTransaction();
+    throw new Error(error);
+  } finally {
+    await clientSession.endSession();
+  }
+};
+
+const getUserById = async (id) => {
+  clientSession.startTransaction();
+  try {
+    const db = await connectDB();
+    const user = db
+      .collection("Accounts")
+      .aggregate([{ $match: { _id: new ObjectId(id) } }]);
+    await clientSession.commitTransaction();
+    return user;
+  } catch (error) {
+    await clientSession.abortTransaction();
+    throw new Error(error);
+  } finally {
+    await clientSession.endSession();
+  }
+};
+
+const createUser = async (username, password) => {
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const addUsertoDB = await db.collection("Accounts").aggregate([
+      {
+        $set: { username, password: hashedPassword },
+      },
+    ]);
+    return addUsertoDB.insertedId;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = { getUserById, getUserByUsername, createUser };
